3edb3390a025871232dc2d8f15896d00f9386b8f
phabets <phabets@student.42.fr>
2023-02-03 13:32:14 +0100

    oui

--- cpp6/ex00/main.cpp
+++ cpp6/ex00/main.cpp
@@ -2,10 +2,13 @@
 #include <iomanip>
 #include <climits>
 #include <stdlib.h>
+#include <string.h>
 
-void	toChar(double val)
+void	toChar(char *str, double val)
 {
-	if (val != val || !isascii(val))
+	if (strlen(str) == 1 && isalpha(str[0]))
+		std::cout << "'" << str[0] << "'" << std::endl;		
+	else if (val != val || !isascii(val))
 		std::cout << "Impossible" << std::endl;
 	else
 	{
@@ -17,19 +20,28 @@ void	toChar(double val)
 	}
 }
 
-void	toInt(double val)
+void	toInt(double val, char *str)
 {
-	int ret = static_cast<int>(val);
-	if (val != val || (val > INT_MAX || val < INT_MIN))
+	if (strlen(str) == 1 && isalpha(str[0]))
+		std::cout << "'" << static_cast<int>(str[0]) << "'" << std::endl;
+	else if (val != val || (val > INT_MAX || val < INT_MIN))
 		std::cout << "Impossible" << std::endl;
 	else
+	{
+		int ret = static_cast<int>(val);
 		std::cout << ret << std::endl;
+	}
 }
 
-void	toFloat(double val)
+void	toFloat(double val, char *str)
 {
-	float ret = static_cast<float>(val);
-	std::cout << ret << "f" << std::endl;
+	if (strlen(str) == 1 && isalpha(str[0]))
+		std::cout << "'" << static_cast<float>(str[0]) << "f'" << std::endl;
+	else
+	{
+		float ret = static_cast<float>(val);
+		std::cout << ret << "f" << std::endl;
+	}
 }
 
 int main(int ac, char **av)
@@ -40,19 +52,22 @@ int main(int ac, char **av)
 		return (0);
 	}
 	double val = atof(av[1]);
+	
 
 	std::cout << std::setprecision(1);
 	std::cout << std::fixed;
 
 	std::cout << "char: ";
-	toChar(val);
+	toChar(av[1], val);
 	
 	std::cout << "int: ";
-	toInt(val);
+	toInt(val, av[1]);
 	
 	std::cout << "float: ";
-	toFloat(val);
-	
-	std::cout << "double: " << val << std::endl;
+	toFloat(val, av[1]);
+	if (strlen(av[1]) == 1 && isalpha(av[1][0]))
+		std::cout << "double: '" << static_cast<double>(av[1][0]) << "'" << std::endl;
+	else
+		std::cout << "double: " << val << std::endl;
 	return (0);
 }
